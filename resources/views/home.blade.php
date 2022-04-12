36846c28a55824b888ee4626accc37dee11a0d5a
raynardtjhang <raytjhang@gmail.com>
2022-04-12 17:49:53 +0700

    create home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
 
 @section('content')
-    <p>Tes ini home</p>
+    <div class="container">
+        <h1 class="text-center">Find Your Future Home</h1>
+        <form action="/search" method="GET">
+            @csrf
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Enter a City, Property, Buy or Rent"
+                    aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+            </div>
+        </form>
+    </div>
 @endsection
