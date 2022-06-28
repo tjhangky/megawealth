866bfe6010722d92b2cf5d9d00f4cd2c6284960a
WINTEOEO <99707715+WINTEOEO@users.noreply.github.com>
2022-06-28 17:18:02 +0700

    test

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -6,8 +6,8 @@
         <p>Our company was founded at 2008 by our founder Renanda. At that time, we started as law firm specializing in real
             estate and cnostruction. In 2012, our company expanded our serivce to real estates with the included service of
             real estates lawyers. Today, our company have 5 offices throughout the states and is planning to build more.</p>
-
-        <h4>Our Offices</h4>
+        
+        <h4>Our Offices halo</h4>
         <div class="d-flex">
             @foreach ($offices as $office)
                 <div class="card mx-1" style="width: 18rem;">
@@ -18,7 +18,6 @@
                         {{-- NANTI INI DI DELETE --}}
                         <img class="card-img-top" src="https://source.unsplash.com/1600x900/?bulding" alt="Card image cap">
                     @endif
-
                     <div class="card-body">
                         <h5 class="card-title">{{ $office->name }}</h5>
                         <p class="card-text ">{{ $office->address }}</p>
