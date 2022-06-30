c29ad6b49d93ccd5589fa6352ac46d323f81c3ef
WINTEOEO <99707715+WINTEOEO@users.noreply.github.com>
2022-06-30 16:57:35 +0700

    seeder

--- database/seeders/OfficeSeeder.php
+++ database/seeders/OfficeSeeder.php
@@ -20,7 +20,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Patriot Baja No.3',
             'contact_name' => 'James Arifin',
             'contact_phone' => '0812-1436-5830',
-            'image' => '',
+            'image' => 'office-1.png',
         ]);
 
         Office::create([
@@ -28,7 +28,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Raya Kebumen No.55',
             'contact_name' => 'Viktor Priyadi',
             'contact_phone' => '0812-3456-7890',
-            'image' => '',
+            'image' => 'office-2.png',
         ]);
 
         Office::create([
@@ -36,31 +36,63 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Kebon Jeruk Raya No.22',
             'contact_name' => 'Jansen Tomat',
             'contact_phone' => '0812-3456-7890',
-            'image' => '',
+            'image' => 'office-3.png',
         ]);
         
         Office::create([
             'name' => 'Pomala Office',
-            'address' => 'Jl. Raya Kedungwringin No.4',
+            'address' => 'Jl. Haji Agus Salim No.4',
             'contact_name' => 'Merry Poniyem',
             'contact_phone' => '0812-3456-7890',
-            'image' => '',
+            'image' => 'office-4.png',
         ]);
 
         Office::create([
             'name' => 'Manokwari Office',
-            'address' => 'Jl. Raya Kedungwringin No.5',
+            'address' => 'Jl. Mangunsakoro No.5',
             'contact_name' => 'Maria ',
             'contact_phone' => '0812-3456-7890',
-            'image' => '',
+            'image' => 'office-5.png',
         ]);
 
         Office::create([
             'name' => 'Padang Office',
-            'address' => 'Jl. Raya Kedungwringin No.6',
+            'address' => 'Jl. Bypass No.16',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => '',
+            'image' => 'office-6.png',
+        ]);
+
+        Office::create([
+            'name' => 'Bekasi Office',
+            'address' => 'Jl. Galaxy No.7',
+            'contact_name' => 'Jeremy',
+            'contact_phone' => '0812-3456-7890',
+            'image' => 'office-7.png',
+        ]);
+
+        Office::create([
+            'name' => 'Bandung Office',
+            'address' => 'Jl. Otista No.15',
+            'contact_name' => 'Jeremy',
+            'contact_phone' => '0812-3456-7890',
+            'image' => 'office-8.png',
+        ]);
+
+        Office::create([
+            'name' => 'Pekanbaru Office',
+            'address' => 'Jl. Ahmad Yani No.21',
+            'contact_name' => 'Jeremy',
+            'contact_phone' => '0812-3456-7890',
+            'image' => 'office-9.png',
+        ]);
+
+        Office::create([
+            'name' => 'Surabaya Office',
+            'address' => 'Jl. Sudirman No.9',
+            'contact_name' => 'Jeremy',
+            'contact_phone' => '0812-3456-7890',
+            'image' => 'office-10.png',
         ]);
     }
 }
