09467a77c4d424039db9a8dea48edb7cff5b8bba
raynardtjhang <raytjhang@gmail.com>
2022-07-01 15:05:27 +0700

    fix image path seeder

--- database/seeders/OfficeSeeder.php
+++ database/seeders/OfficeSeeder.php
@@ -20,7 +20,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Patriot Baja No.3',
             'contact_name' => 'James Arifin',
             'contact_phone' => '0812-1436-5830',
-            'image' => 'office-1.png',
+            'image' => 'office-images/office-1.png',
         ]);
 
         Office::create([
@@ -28,7 +28,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Raya Kebumen No.55',
             'contact_name' => 'Viktor Priyadi',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-2.png',
+            'image' => 'office-images/office-2.png',
         ]);
 
         Office::create([
@@ -36,7 +36,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Kebon Jeruk Raya No.22',
             'contact_name' => 'Jansen Tomat',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-3.png',
+            'image' => 'office-images/office-3.png',
         ]);
         
         Office::create([
@@ -44,7 +44,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Haji Agus Salim No.4',
             'contact_name' => 'Merry Poniyem',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-4.png',
+            'image' => 'office-images/office-4.png',
         ]);
 
         Office::create([
@@ -52,7 +52,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Mangunsakoro No.5',
             'contact_name' => 'Maria ',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-5.png',
+            'image' => 'office-images/office-5.png',
         ]);
 
         Office::create([
@@ -60,7 +60,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Bypass No.16',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-6.png',
+            'image' => 'office-images/office-6.png',
         ]);
 
         Office::create([
@@ -68,7 +68,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Galaxy No.7',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-7.png',
+            'image' => 'office-images/office-7.png',
         ]);
 
         Office::create([
@@ -76,7 +76,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Otista No.15',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-8.png',
+            'image' => 'office-images/office-8.png',
         ]);
 
         Office::create([
@@ -84,7 +84,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Ahmad Yani No.21',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-9.png',
+            'image' => 'office-images/office-9.png',
         ]);
 
         Office::create([
@@ -92,7 +92,7 @@ class OfficeSeeder extends Seeder
             'address' => 'Jl. Sudirman No.9',
             'contact_name' => 'Jeremy',
             'contact_phone' => '0812-3456-7890',
-            'image' => 'office-10.png',
+            'image' => 'office-images/office-10.png',
         ]);
     }
 }
