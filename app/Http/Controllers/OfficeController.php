b0e021e0b67a989a86a88d88d3e1fd8e8af42769
raynardtjhang <raytjhang@gmail.com>
2022-07-19 16:07:58 +0700

    about us restrict admin

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Office;
+use Illuminate\Support\Facades\Gate;
 
 class OfficeController extends Controller
 {
     public function index()
     {
+        if (Gate::allows('admin')) {
+            abort(403, 'Unauthorized access.');
+        }
+
         $offices = Office::paginate(5);
         $active = 'about-us';
         return view('about-us', compact('offices', 'active'));
